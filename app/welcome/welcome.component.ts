96ae7cd7aa3593f1b88fe9815d98a7900fa8b81c
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2016-12-04 19:04:54 +0530

    Done #18

--- app/welcome/welcome.component.ts
+++ app/welcome/welcome.component.ts
@@ -3,7 +3,8 @@ import {Component} from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'bs-welcome',
-  templateUrl: 'welcome.component.html'
+  templateUrl: 'welcome.component.html',
+  styleUrls: ['welcome.component.css']
 })
 
 export class WelcomeComponent {
